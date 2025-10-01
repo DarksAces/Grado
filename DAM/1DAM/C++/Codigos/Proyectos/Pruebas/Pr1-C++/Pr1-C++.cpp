0885ffbaf117d9e65c09fbcf8727561f222e4a23
Daniel <166744714+DarksAces@users.noreply.github.com>
2025-10-01 13:33:06 +0200

    Update

--- DAM/1DAM/C++/Codigos/Proyectos/Pruebas/Pr1-C++/Pr1-C++.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "config.h"
-
-int main(int argc, char **argv) {
-	std::cout << "Hello World" << std::endl;
-	std::cout << "Version " << Pr1-C++_VERSION_MAJOR << "." << Pr1-C++_VERSION_MINOR << std::endl;
-	return 0;
-}
