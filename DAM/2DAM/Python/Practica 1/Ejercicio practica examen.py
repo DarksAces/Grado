edaab655db774b043414a4dbf9aef5c873ab4d08
Daniel <166744714+DarksAces@users.noreply.github.com>
2025-10-08 09:58:19 +0200

    Create Ejercicio practica examen.py

---
+++ DAM/2DAM/Python/Practica 1/Ejercicio practica examen.py
@@ -0,0 +1,28 @@
+print(3 * 'ab')
+
+print('Línea1', end=' ')
+print('Línea2')
+
+
+for x in range(3):
+ if x == 1:
+    continue
+ print(x)
+ 
+for i in range(1, 6):
+ print(i)
+ 
+ 
+ 
+nombre = input('Introduce tu nombre:')
+
+print(f'hola {nombre}!')
+
+
+for n in range(4):
+ if n == 2:
+    pass
+ else:
+    print(n)
+    
+    
