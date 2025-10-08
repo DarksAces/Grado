9d39b194c595e32673431ca4168cca8f7e4208a0
Daniel <166744714+DarksAces@users.noreply.github.com>
2025-10-08 10:03:48 +0200

    Update Ejercicio practica examen.py

--- DAM/2DAM/Python/Practica 1/Ejercicio practica examen.py
+++ DAM/2DAM/Python/Practica 1/Ejercicio practica examen.py
@@ -1,28 +1,54 @@
+print('Hola', 'mundo', sep='-')
+
+
+for i in range(3): 
+   pass
+
+
 print(3 * 'ab')
+
+
+persona = {'nombre': 'Ana', 'edad': 30, 'ciudad': 'Madrid'}
+
+
 
 print('Línea1', end=' ')
 print('Línea2')
 
 
 for x in range(3):
- if x == 1:
-    continue
- print(x)
- 
+    if x == 1:
+        continue
+    print(x)
+
+
+
 for i in range(1, 6):
- print(i)
- 
- 
- 
-nombre = input('Introduce tu nombre:')
-
-print(f'hola {nombre}!')
+    print(i)
 
 
-for n in range(4):
- if n == 2:
-    pass
- else:
-    print(n)
-    
-    
+nombre = input("Introduce tu nombre: ")
+print(f'Hola {nombre}!')
+
+
+
+
+
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        return f'Hola, me llamo {self.nombre}!'
+
+
+
+x = int(input("Introduce un número: "))
+
+match x:
+    case 1:
+        print("uno")
+    case 2:
+        print("dos")
+    case _:
+        print("otro")
