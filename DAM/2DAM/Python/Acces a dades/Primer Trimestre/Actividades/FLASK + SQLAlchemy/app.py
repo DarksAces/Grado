7eb7bbecd10691797dcc47bbaaa839d3b284e60a
DarksAces <166744714+DarksAces@users.noreply.github.com>
2025-12-10 10:21:08 +0100

    Update

---
+++ DAM/2DAM/Python/Acces a dades/Primer Trimestre/Actividades/FLASK + SQLAlchemy/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+
+with app.app_context():
+    db.create_all()
+
+class BookSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Book
+
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
+
+@app.route('/book', methods=['POST'])
+def add_book():
+    new_book = Book(**request.json)
+    db.session.add(new_book)
+    db.session.commit()
+    return book_schema.jsonify(new_book), 201
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    return books_schema.jsonify(Book.query.all())
+
+@app.route('/book/<int:id>', methods=['GET'])
+def get_book(id):
+    return book_schema.jsonify(Book.query.get_or_404(id))
+
+@app.route('/book/<int:id>', methods=['PUT'])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    for key, value in request.json.items():
+        setattr(book, key, value)
+    db.session.commit()
+    return book_schema.jsonify(book)
+
+@app.route('/book/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({"message": "Eliminado"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
